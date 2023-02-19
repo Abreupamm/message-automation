8da34d84aadb3e40864f7414d8c48f0d2f478cb9
Pamela <abrepamm@gmail.com>
2023-02-18 21:34:44 -0300

    feat: função para enviar mensagem implementada

--- src/main.py
+++ src/main.py
@@ -1,23 +1,47 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
 import csv
 import time
+import urllib
 
 
 def import_data(path: str):
-    mesage = input('Digite sua mensagem:')
     if '.csv' in path:
         with open(path) as file:
             archive = csv.DictReader(file, delimiter=',', quotechar='"')
-            print(mesage)
             return list(archive)
     else:
         raise ValueError('Arquivo inválido')
 
 
-def execut_driver():
-    driver = webdriver.Chrome(executable_path=r'src/chromedriver')
-    driver.get('https://web.whatsapp.com/')
+contacts_list = import_data('src/aqr.csv')
+
+mesage = input('Digite sua mensagem:')
+text = urllib.parse.quote(mesage)
+
+service = FirefoxService(GeckoDriverManager().install())
+driver = webdriver.Firefox(service=service)
+driver.get('https://web.whatsapp.com/')
 
 
-print(import_data('src/aqr.csv'))
+while len(driver.find_elements(By.ID, 'side')) < 1:
+    time.sleep(2)
+
+
+for phone_number in contacts_list:
+    number = phone_number['Telefone']
+    link = f'https://web.whatsapp.com/send?phone=55{number}&text={text}'
+    driver.get(link)
+    while len(driver.find_elements(By.CSS_SELECTOR, '._3E8Fg')) < 1:
+        time.sleep(1)
+
+    
+    time.sleep(5)
+    button_element = driver.find_element(By.CSS_SELECTOR, 'button.tvf2evcx')
+    button_element.click()
+
+driver.close()
+print('FIM')
