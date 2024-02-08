c1b12dc456b07f1ee19bcfeae4905b30f194f727
Abreupamm <abreupamm@gmail.com>
2024-02-07 22:34:42 -0300

    resolução de bug: envio da mensagem para os contatos depois de salvar o número

--- src/main.py
+++ src/main.py
@@ -11,6 +11,13 @@ from selenium.webdriver.support import expected_conditions as EC
 import tkinter as tk
 from tkinter import filedialog
 
+service = FirefoxService(GeckoDriverManager().install())
+driver = webdriver.Firefox(service=service)
+
+def format_number(n):
+    number_formatted = f"+55 {n[:2]} {n[2:7]}-{n[7:]}"
+
+    return number_formatted
 
 def import_data(path: str):
     if '.csv' in path:
@@ -20,35 +27,51 @@ def import_data(path: str):
     else:
         raise ValueError('Arquivo inválido')
 
-def send_message():
+def save_number(number):
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f"//a[text()='{number}']")))
+    link_number = driver.find_element(By.XPATH, f"//a[text()='{number}']")
+    link_number.click()
+    number_formatted = format_number(number)
+
+    WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, f"//span[text()='{number_formatted}']")))
+    time.sleep(2)
+    button_link_number = driver.find_element(By.XPATH, f"//span[text()='{number_formatted}']")
+    button_link_number.click()
+
+def send_message(message, number):
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, '_3Uu1_')))
+    input_mensage = driver.find_elements(By.CLASS_NAME, '_3Uu1_')[0]
+    input_mensage.click()
+   
+    for m in message:
+        input_mensage.send_keys(m)
+    
+
+    button_element = driver.find_element(By.CSS_SELECTOR, 'button.tvf2evcx')
+    button_element.click()
+
+
+def main():
     contacts_list = import_data('src/aqr_exemple.csv')
 
     mesage = entry_message.get()
-    text = urllib.parse.quote(mesage)
-
-    service = FirefoxService(GeckoDriverManager().install())
-    driver = webdriver.Firefox(service=service)
+    # text = urllib.parse.quote(mesage)
     driver.get('https://web.whatsapp.com/')
 
 
     while len(driver.find_elements(By.ID, 'side')) < 1:
         time.sleep(2)
 
-    element_group = driver.find_elements(By.CSS_SELECTOR, 'span[title=TESTE]')
-    element_group[0].click()
 
     for phone_number in contacts_list:
-
+        element_group = driver.find_elements(By.CSS_SELECTOR, 'span[title=TESTE]')
+        element_group[0].click()
         number = phone_number['Telefone']
-        # link = f'https://web.whatsapp.com/send?phone=55{number}&text={text}'
-        # driver.get(link)
-        # while len(driver.find_elements(By.CSS_SELECTOR, '._3E8Fg')) < 1:
-        #     time.sleep(1)
-        input_mensage = driver.find_elements(By.CLASS_NAME, 'selectable-text copyable-text iq0m558w g0rxnol2')
-        input_mensage[0].send_keys(number)
-        button_element = driver.find_element(By.CSS_SELECTOR, 'button.tvf2evcx')
-        button_element.click()
-        time.sleep(5)
+
+        send_message(number, True)
+        save_number(number)
+        time.sleep(10)
+        send_message(mesage, False)
 
     # driver.close()
     print('FIM')
@@ -60,7 +83,7 @@ root.title('Envio de Mensagens WhatsApp')
 # Elementos da Interface
 label_message = tk.Label(root, text='Digite sua mensagem:')
 entry_message = tk.Entry(root, width=50)
-button_send = tk.Button(root, text='Enviar Mensagens', command=send_message)
+button_send = tk.Button(root, text='Enviar Mensagens', command=main)
 
 # Posicionamento dos Elementos
 label_message.grid(row=0, column=0, padx=10, pady=10)
