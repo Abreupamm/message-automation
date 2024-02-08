9eac92d031fa04cc50a2004f382a2c57dca0565d
Abreupamm <abreupamm@gmail.com>
2024-02-08 15:30:55 -0300

    remoção de comentários e ajuste de tempo

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import tkinter as tk
 from tkinter import filedialog
-
 service = FirefoxService(GeckoDriverManager().install())
 driver = webdriver.Firefox(service=service)
 
@@ -34,12 +33,12 @@ def save_number(number):
     number_formatted = format_number(number)
 
     WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, f"//span[text()='{number_formatted}']")))
-    time.sleep(2)
     button_link_number = driver.find_element(By.XPATH, f"//span[text()='{number_formatted}']")
     button_link_number.click()
 
 def send_message(message, number):
     WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, '_3Uu1_')))
+    time.sleep(1)
     input_mensage = driver.find_elements(By.CLASS_NAME, '_3Uu1_')[0]
     input_mensage.click()
    
@@ -52,10 +51,9 @@ def send_message(message, number):
 
 
 def main():
-    contacts_list = import_data('src/aqr_exemple.csv')
+    contacts_list = import_data('lista-de-clentes.csv')
 
-    mesage = entry_message.get()
-    # text = urllib.parse.quote(mesage)
+    mesage = entry_message.get("1.0", tk.END)
     driver.get('https://web.whatsapp.com/')
 
 
@@ -64,17 +62,19 @@ def main():
 
 
     for phone_number in contacts_list:
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span[title=TESTE]')))
         element_group = driver.find_elements(By.CSS_SELECTOR, 'span[title=TESTE]')
         element_group[0].click()
         number = phone_number['Telefone']
 
         send_message(number, True)
         save_number(number)
-        time.sleep(10)
+        time.sleep(2)
         send_message(mesage, False)
 
-    # driver.close()
-    print('FIM')
+    driver.close()
+    fim = tk.Label(root, text='FIM')
+    fim.grid(row=2, column=0, columnspan=2, pady=10)
 
 # Interface Gráfica
 root = tk.Tk()
@@ -82,13 +82,13 @@ root.title('Envio de Mensagens WhatsApp')
 
 # Elementos da Interface
 label_message = tk.Label(root, text='Digite sua mensagem:')
-entry_message = tk.Entry(root, width=50)
+entry_message = tk.Text(root, width=50, height=10)
 button_send = tk.Button(root, text='Enviar Mensagens', command=main)
 
 # Posicionamento dos Elementos
 label_message.grid(row=0, column=0, padx=10, pady=10)
 entry_message.grid(row=0, column=1, padx=10, pady=10)
-button_send.grid(row=2, column=0, columnspan=2, pady=10)
+button_send.grid(row=3, column=0, columnspan=2, pady=10)
 
 # Execução da Interface
 root.mainloop()
